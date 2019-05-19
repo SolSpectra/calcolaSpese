8fb544d7b16b406e28b6cee2dc91b85981a767c8
Giulietta <x.giulietta93.x@gmail.com>
2019-05-19 14:47:08 +0200

    first commit

---
+++ src/main/java/com/spectra/calcolaSpese/CalcolaSpeseApplication.java
@@ -0,0 +1,13 @@
+package com.spectra.calcolaSpese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcolaSpeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcolaSpeseApplication.class, args);
+	}
+
+}
